3999419589f4b5bb62bf0b14cc72eb157f451177
alessandrodanescu96 <aaalessandrodanescu@gmail.com>
2022-10-28 14:27:10 +0300

    Week 7

---
+++ Code/Week7/posts.js
@@ -0,0 +1,73 @@
+function runWeatherApp() {
+    var weatherKey = "3e20f53fb2ef40e28bf95905221510";
+    var weatherBaseUrl = "https://api.weatherapi.com/v1";
+  
+    var temperatureButtonEl = document.querySelector("#temperature-button");
+    temperatureButtonEl.addEventListener("click", onTemperatureButtonClick);
+    var temperatureContainerEl = document.querySelector("#temperature-container");
+  
+    function onTemperatureButtonClick() {
+      var queryParams = "key=" + weatherKey + "&q=Bucharest";
+      fetch(weatherBaseUrl + "/current.json?" + queryParams)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (weather) {
+          var myWeather = weather || {};
+          var currentWeather = myWeather.current || {};
+          var temperature = currentWeather.temp_c || null;
+          if (temperature !== null) {
+            temperatureContainerEl.style = null;
+            var temperatureEl =
+              temperatureContainerEl.querySelector("#temperature");
+            temperatureEl.innerHTML = temperature;
+          }
+        });
+    }
+  }
+  
+  function runPostsApp() {
+    var postsBaseUrl = "https://jsonplaceholder.typicode.com";
+    var postsContainerEl = document.querySelector("#posts-container");
+  
+    function renderPostList(posts) {
+      posts.forEach(function (post) {
+        var postDiv = createPost(post);
+        if (postDiv) {
+          postsContainerEl.appendChild(postDiv);
+        }
+      });
+    }
+  
+    function createPost(post) {
+      var div = document.createElement("div");
+      //post.id
+      div.innerHTML =
+        "<div class='post-title'>" +
+        "<h2>" +
+        post.title +
+        "</h2>" +
+        "</div><p class='post-body'>" +
+        post.body +
+        "</p>";
+  
+      return div;
+    }
+  
+    fetch(postsBaseUrl + "/posts")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (posts) {
+        renderPostList(posts);
+      });
+  }
+  
+  var pageInit = function () {
+    runWeatherApp();
+    runPostsApp();
+  };
+  
+  window.addEventListener("load", function () {
+    pageInit();
+  });
